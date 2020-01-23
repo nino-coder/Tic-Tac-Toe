2504c7ce9c8a4c56a18df0b0e5e3b2aaae464661
nino-coder <nino.giorgadze.2000@gmail.com>
2020-01-23 22:43:49 +0400

    with some changes

--- src/components/js_files/game.js
+++ src/components/js_files/game.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import TicTacToe from './ticTacToe';
 import game from '../css_files/game.css'
 import chooseTable from './chooseTable';
-import Board from './ticTacToe';
+import Board from './ticTacToeOther';
 
 export default class Game extends TicTacToe {
     constructor(props) {
@@ -10,24 +10,24 @@ export default class Game extends TicTacToe {
         this.state = {
             xIsNext: true,
             stepNumber: 0,
-            row:this.props.row,
+            row: this.props.row,
             history: [
-                { squares: Array(this.props.row*this.props.row).fill(null) }
+                { squares: Array(this.props.row * this.props.row).fill(null) }
             ]
         }
     }
-    jumpTo(step){
+    jumpTo(step) {
         this.setState({
             stepNumber: step,
             xIsNext: true
         })
     }
     handleClick(i) {
-        const history = this.state.history.slice(0, this.state.stepNumber + 1); 
-        const current = history[history.length - 1]; 
-        const squares = current.squares.slice(); 
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
         console.log(squares)
-        const winner = Winner(squares);
+        const winner = this.win(squares);
         if (winner || squares[i]) {
             return;
         }
@@ -36,21 +36,39 @@ export default class Game extends TicTacToe {
             history: history.concat({
                 squares: squares
             }),
-            xIsNext: !this.state.xIsNext, 
-            stepNumber: history.length 
+            xIsNext: !this.state.xIsNext,
+            stepNumber: history.length
         });
+        
 
     }
-    
-  
+    win(squares){
+        for(let i=0;i<this.props.row;i++){   
+            for(let k=i*this.props.row;k<i*this.props.row+this.props.row;k++){
+                switch(this.props.row){
+                    case 2:
+                        return squares[k]===squares[k+1];
+                    case 3:
+                        return squares[k]===squares[k+1]&& squares[k+1]===squares[k+2]
+                    case 4:
+                        return squares[k]=== squares[k+1]&& squares[k+1]===squares[k+2]&& squares[k+2]===squares[k+3]
+                    default:
+                        return  null;
+                }  
+              
+                       
+            } 
+        }
+        return null;
+    }
     render() {
         const history = this.state.history;
         const current = history[this.state.stepNumber];
-        const winner = Winner(current.squares);
+        const winner = this.win(current.squares);
         const moves = history.map((step, move) => {
             const desc = move ? 'Go to #' + move : 'Start the Game';
             return (
-                <li className ="goTo"key={move}>
+                <li className="goTo" key={move}>
                     <button onClick={() => { this.jumpTo(move) }}>
                         {desc}
                     </button>
@@ -69,41 +87,51 @@ export default class Game extends TicTacToe {
 
         return (
             <div className="gameContainer">
-               <div className="game">
-                <div className="game-info">
-                    <p>{status}</p>
-                </div>
-                <div className="game-board">
-                    <Board row={this.props.row} onClick={(i) => this.handleClick(i)}
-                        squares={current.squares} />
-                </div>
+                <div className="game">
+                    <div className="game-info">
+                        <p>{status}</p>
+                    </div>
+                    <div className="game-board">
+                        <Board row={this.props.row} onClick={(i) => this.handleClick(i)}
+                            squares={current.squares} />
+                    </div>
                     <ul>{moves}</ul>
-               </div> </div>
+                </div> </div>
         )
     }
 }
 
+// function win(squares){
+//     for(let i=0;i<this.props.row*this.props.row;i++){
+//         for(let k=i*this.props.row;k<i*this.props.row+this.props.row;k++){
+//             if(squares[k]===squares[k+1]){
+//                 return squares[k];
+//             }
+//         }
+//     }
+//     return null;
+// }
 
-function Winner(squares) {
-    const lines = [
-        [0, 1, 2,3],
-        [4, 5,6,7],
-        [8,9,10,11],
-        [12,13,14,15],
-        [0,4,8,12],
-        [1,5,9,13],
-        [2,6,10,14],
-        [3,7,11,15],
-        [0,5,10,15],
-        [3,6,9,12]
-    ];
-    for (let i = 0; i < lines.length; i++) {
-        const [a, b, c,d] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c] && squares[c] === squares[d]) {
-            return squares[a];
-        }
-    }
-   
+// function Winner(squares) {
+//     const lines = [
+//         [0, 1, 2, 3],
+//         [4, 5, 6, 7],
+//         [8, 9, 10, 11],
+//         [12, 13, 14, 15],
+//         [0, 4, 8, 12],
+//         [1, 5, 9, 13],
+//         [2, 6, 10, 14],
+//         [3, 7, 11, 15],
+//         [0, 5, 10, 15],
+//         [3, 6, 9, 12]
+//     ];
+//     for (let i = 0; i < lines.length; i++) {
+//         const [a, b, c, d] = lines[i];
+//         if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c] && squares[c] === squares[d]) {
+//             return squares[a];
+//         }
+        
+//     }
 
-    return null;
-}
+//     return null;
+// }
