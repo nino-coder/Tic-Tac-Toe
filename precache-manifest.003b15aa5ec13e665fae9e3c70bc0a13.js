c423e37088cc056d3753e27347fd286ae09a7581
nino-coder <nino.giorgadze.2000@gmail.com>
2020-04-30 19:09:57 +0400

    Updates

---
+++ precache-manifest.003b15aa5ec13e665fae9e3c70bc0a13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f9074b6dc7826f241766b977f4e485d",
+    "url": "/Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "02de2bfc8f6aad9c9fec",
+    "url": "/Tic-Tac-Toe/static/css/main.19bcd325.chunk.css"
+  },
+  {
+    "revision": "3ce4278dde17b35ef6de",
+    "url": "/Tic-Tac-Toe/static/js/2.970ef885.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Tic-Tac-Toe/static/js/2.970ef885.chunk.js.LICENSE"
+  },
+  {
+    "revision": "02de2bfc8f6aad9c9fec",
+    "url": "/Tic-Tac-Toe/static/js/main.6a249a1b.chunk.js"
+  },
+  {
+    "revision": "d6e5cd3e8af3deedf3b6",
+    "url": "/Tic-Tac-Toe/static/js/runtime-main.7472c937.js"
+  }
+]);
